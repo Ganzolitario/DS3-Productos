348008aeecf37a43159a18e343c31ebb69fef83d
Ganzolitario <cristian.rivera.torres@correounivalle.edu.co>
2025-03-31 22:38:28 -0500

    Product microservice is done, connection with the API gateway remaining

--- src/main/java/com/example/spring_api/Product.java
+++ src/main/java/com/example/spring_api/Product.java
@@ -1,23 +1,35 @@
 package com.example.spring_api;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.*;
 import lombok.Data;
+import java.math.BigDecimal;
+import java.sql.Timestamp;
 
-import java.time.LocalDate;
-
-@Entity
 @Data
+@Entity
+@Table(name = "productos")
 public class Product {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long id;
-    String name;
-    String description;
-    String brand;
-    LocalDate acquisitionDate;
-    double price;
+    @Column(name = "id_producto")
+    private Long idProducto;
+
+    @Column(name = "nombre_producto", nullable = false, length = 100)
+    private String nombreProducto;
+
+    @Column(name = "descripcion")
+    private String descripcion;
+
+    @Column(name = "precio", nullable = false)
+    private BigDecimal precio;
+
+    @Column(name = "stock", nullable = false)
+    private Integer stock;
+
+    @Column(name = "categoria", length = 50)
+    private String categoria;
+
+    @Column(name = "fecha_creacion", updatable = false)
+    private Timestamp fechaCreacion;
 }
